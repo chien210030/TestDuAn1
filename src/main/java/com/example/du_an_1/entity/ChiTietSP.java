89724a3960bdff52961022aad0f5873c035ec78c
chientqph25370 <118548576+chientqph25370@users.noreply.github.com>
2023-07-12 23:44:52 +0700

    no message

--- src/main/java/com/example/du_an_1/entity/ChiTietSP.java
+++ src/main/java/com/example/du_an_1/entity/ChiTietSP.java
@@ -1,4 +1,31 @@
 package com.example.du_an_1.entity;
 
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "chitietsp")
 public class ChiTietSP {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String ma;
+    private String ten;
+    private String mota;
+    private int  soluongton;
+    private BigDecimal giaNhap;
+    private BigDecimal giaBan;
+//    @ManyToOne
+//    @JoinColumn(name = "idchatlieu")
+//    private ChatLieu chatLieu;
+//
+//
+//    @ManyToOne
+//@JoinColumn(name = "idthuonghieu")
+//    private  ThuongHieu thuongHieu;
+
+
+
 }
