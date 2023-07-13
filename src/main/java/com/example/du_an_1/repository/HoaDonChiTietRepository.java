4d7295a2ad8862923bd42a5706b8cd985001d06d
HHMANH <Manhhhph28585@fpt.edu.vn>
2023-07-13 12:28:31 +0700

    Create HDCT

---
+++ src/main/java/com/example/du_an_1/repository/HoaDonChiTietRepository.java
@@ -0,0 +1,8 @@
+package com.example.du_an_1.repository;
+
+import com.example.du_an_1.entity.HoaDonChiTiet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet, Integer> {
+
+}
