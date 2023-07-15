9a49f8be66dc2c3283871bb8c58a09911d9da82d
ChangXinh23 <trangnthph24914@fpt.edu.vn>
2023-07-15 15:19:27 +0700

    chang comit

--- src/main/java/com/example/du_an_1/controller/KhachHangController.java
+++ src/main/java/com/example/du_an_1/controller/KhachHangController.java
@@ -1,7 +1,6 @@
 package com.example.du_an_1.controller;
 
 import com.example.du_an_1.entity.KhachHang;
-import com.example.du_an_1.entity.NhanVien;
 import com.example.du_an_1.repository.KhachHangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,10 +8,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -33,16 +35,85 @@ public class KhachHangController {
     @GetMapping("/khach-hang/remove/{id}")
     public String remove(@PathVariable("id") Integer id){
         khachHangRepository.deleteById(id);
-        return "redirect:/KhachHang/index";
+        return "redirect:/khach-hang/hien-thi";
     }
 
     @GetMapping("/khach-hang/view-update/{id}")
-    public String viewUpdate(Model model,@PathVariable("id") Integer id){
+    public String viewUpdate(Model model, @PathVariable("id") Integer id){
 
-        KhachHang kh = khachHangRepository.getReferenceById(id);
-        model.addAttribute("update",kh);
+        KhachHang khachHang = khachHangRepository.findById(Integer.valueOf(id)).orElse(null);
+        model.addAttribute("update", khachHang);
 
         return "/KhachHang/viewUpdate";
+    }
+
+    @GetMapping("/khach-hang/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+
+        KhachHang khachHang = khachHangRepository.findById(Integer.valueOf(id)).orElse(null);
+        model.addAttribute("dt", khachHang);
+
+        return "/KhachHang/detailKH";
+    }
+
+
+    @PostMapping("/khach-hang/update")
+    public String update(Model model,
+                         @RequestParam("id") Integer id,
+                         @RequestParam("ho") String ho,
+                         @RequestParam("tendem") String tendem,
+                         @RequestParam("ten") String ten,
+                         @RequestParam("gioitinh") Boolean gioitinh,
+                         @RequestParam("ngaysinh") Date ngaysinh,
+                         @RequestParam("email") String email,
+                         @RequestParam("sdt") String sdt,
+                         @RequestParam("diemthuong") Integer diemthuong
+    ){
+
+
+        KhachHang khachHang = khachHangRepository.findById(Integer.valueOf(id)).orElse(null);
+
+        khachHang.setHo(ho);
+        khachHang.setTendem(tendem);
+        khachHang.setTen(ten);
+        khachHang.setGioitinh(gioitinh);
+        khachHang.setNgaysinh(ngaysinh);
+        khachHang.setEmail(email);
+        khachHang.setSdt(sdt);
+        khachHang.setDiemthuong(diemthuong);
+
+        khachHangRepository.save(khachHang);
+
+        return "redirect:/khach-hang/hien-thi";
+    }
+
+    @GetMapping("/khach-hang/view-add")
+    public String viewAdd(Model model, @Validated @ModelAttribute("kh") KhachHang kh , BindingResult result){
+        model.addAttribute("kh",new KhachHang());
+        return "/KhachHang/ViewAdd";
+    }
+
+
+
+    @PostMapping("/khach-hang/add")
+    public String add(Model model, @Validated @ModelAttribute("kh") KhachHang kh, BindingResult result){
+
+
+//            if (result.hasErrors()){
+//                return "/KhachHang/ViewAdd";
+//            }
+
+        khachHangRepository.save(kh);
+        return "redirect:/khach-hang/hien-thi";
+    }
+
+
+    @ModelAttribute("gioitinh")
+    public Map<Boolean, String> getGenders() {
+        Map<Boolean, String> map = new HashMap<>();
+        map.put(true, "Nam");
+        map.put(false, "Nữ");
+        return map;
     }
 
 }
--- src/main/java/com/example/du_an_1/entity/KhachHang.java
+++ src/main/java/com/example/du_an_1/entity/KhachHang.java
@@ -1,6 +1,9 @@
 package com.example.du_an_1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.Date;
@@ -16,21 +19,34 @@ import java.util.List;
 public class KhachHang {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Integer id ;
 
+    @NotBlank(message = "Không được để trống tên !")
     private String ten ;
+
+    @NotBlank(message = "Không được để trống tên đệm !")
     private String tendem ;
+
+    @NotBlank(message = "Không được để trống họ !")
     private String ho ;
+
+    @NotNull(message = "Chọn giới tính !")
     private  Boolean gioitinh ;
 
     @Temporal(TemporalType.DATE)
     @Column(name = "ngaysinh")
     Date ngaysinh = new Date();
 
+    @NotBlank(message = "Không được để trống email !")
+    @Email(message = "Email không đúng định dạng !")
     private String email ;
+
+    @NotBlank(message = "Không được để trống sdt !")
     private String sdt ;
+
+    @NotNull(message = "Không được để trống điểm thưởng !")
     private int diemthuong;
 
 
