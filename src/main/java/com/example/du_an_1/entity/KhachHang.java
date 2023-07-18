6e8a4b7fc3c978a8cae6b70b5bb8b53bdf432543
hiepth22 <hiepthph25029@fpt.edu.vn>
2023-07-18 15:16:44 +0700

    Đẩy lại khachhangcontroll lên

--- src/main/java/com/example/du_an_1/entity/KhachHang.java
+++ src/main/java/com/example/du_an_1/entity/KhachHang.java
@@ -8,6 +8,7 @@ import lombok.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,35 +22,41 @@ public class KhachHang {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private Integer id ;
+    private UUID id ;
 
+    @Column(name = "Ten")
     @NotBlank(message = "Không được để trống tên !")
     private String ten ;
 
+    @Column(name = "TenDem")
     @NotBlank(message = "Không được để trống tên đệm !")
     private String tendem ;
 
+    @Column(name = "Ho")
     @NotBlank(message = "Không được để trống họ !")
     private String ho ;
 
+    @Column(name = "GioiTinh")
     @NotNull(message = "Chọn giới tính !")
     private  Boolean gioitinh ;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "ngaysinh")
+    @Column(name = "NgaySinh")
     Date ngaysinh = new Date();
 
+    @Column(name = "Email")
     @NotBlank(message = "Không được để trống email !")
     @Email(message = "Email không đúng định dạng !")
     private String email ;
 
+    @Column(name = "Sdt")
     @NotBlank(message = "Không được để trống sdt !")
     private String sdt ;
 
+    @Column(name = "DiemThuong")
     @NotNull(message = "Không được để trống điểm thưởng !")
     private int diemthuong;
 
-
-    @OneToMany(mappedBy = "khachhang")
+    @OneToMany(mappedBy = "KhachHang")
     List<HoaDon> hoadon;
 }
